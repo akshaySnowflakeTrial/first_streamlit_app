7ede9902e1ad3cc0f6f5fae844d9cd74dd4e900a
akshaySnowflakeTrial <150377558+akshaySnowflakeTrial@users.noreply.github.com>
2023-11-09 14:22:59 -0500

    Update streamlit_app.py
    fixed brackets for fruits_to_show variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,6 @@ streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 # display the table on the page
-fruits_to_show= streamlit.dataframe(my_fruit_list)
+fruits_to_show= streamlit.dataframe[my_fruit_list]
 # display the table on the page
 streamlit.dataframe(fruits_to_show)
